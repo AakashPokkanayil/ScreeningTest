872c50a7bb7399adbfcaadb197314f498177ac4d
Aakash Pokkanayil <aakashpokkanayil@gmail.com>
2022-09-16 01:16:54 +0530

    Add project files.

---
+++ ScreeningTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScreeningTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
